2191666631f85bfaf8e06eab87efe12286e170cc
urjhn <uyennhi01022004@gmail.com>
2025-01-22 08:20:01 +0700

    logo

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from "../../assets/logo.png"
+
+const Navbar = () => {
+  return (<div>
+        {/* upper Navbar */}
+        <div>
+            <div>
+                <div>
+                    <a href="#"></a>
+                    <img src={Logo} alt="Logo"/>
+                </div>
+            </div>
+        </div>
+        {/* lower Navbar */}
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
