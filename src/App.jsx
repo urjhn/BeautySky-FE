d7495d6622ba154dc8661c94b33ddebe3196d9ed
urjhn <uyennhi01022004@gmail.com>
2025-02-01 11:26:10 +0700

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return(
     <div className="overflow-x-hidden">
-       <Navbar />
-       <Hero />
+        <Navbar handleOrderPopup={handleOrderPopup} />
+        <Hero handleOrderPopup={handleOrderPopup} />
       </div>
   )
 }
