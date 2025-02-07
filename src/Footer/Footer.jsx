385453e31efb525a17ad78c91194f018ed84ec29
hoanghai170504 <hailhse183904@fpt.edu.vn>
2025-02-07 07:30:43 +0700

    HoangHai check 7/2

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 import Logo from "../../assets/footer/logo.png";
 
 const Footer = () => {
   return (
     <div>
       <h1 className="sm:text-left text-justify mb-3 flex items-center gap-3">
-              <img src={Logo} alt="Logo" className="w-16" />
-            
-            </h1>
+        <img src={Logo} alt="Logo" className="w-16" />
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
