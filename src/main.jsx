4ba3256923fad8d0292e186d5883b29dbea02b16
Hoàng Hải <haile170504@gmail.com>
2025-02-23 16:40:30 +0700

    Fix Authenticate day 23/2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom"; // Vẫn giữ Router ở đây
 import { CartProvider } from "./context/CartContext.jsx";
 import { ThemeProvider } from "./pages/DashBoard/context/ThemeContext.jsx";
 import { EventProvider } from "./context/EvenContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -10,12 +12,18 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <GoogleOAuthProvider clientId="97056897827-v2rgbcjteb21e5ogji3aff65toeg0bc6.apps.googleusercontent.com">
-    <CartProvider>
-      <ThemeProvider>
-        <EventProvider>
-          <App />
-        </EventProvider>
-      </ThemeProvider>
-    </CartProvider>
+    <Router>
+      {" "}
+      {/* Chỉ có 1 Router ở đây */}
+      <AuthProvider>
+        <CartProvider>
+          <ThemeProvider>
+            <EventProvider>
+              <App />
+            </EventProvider>
+          </ThemeProvider>
+        </CartProvider>
+      </AuthProvider>
+    </Router>
   </GoogleOAuthProvider>
 );
